08f0f3f60345d9741e04be92cd86209cf252a61d
Hanguyen2332 <thanhha2332@gmail.com>
2021-10-22 16:09:32 +0700

    HaNTT commit code for function class room

---
+++ back-end/src/main/java/com/codegym/rest_controller/ClassRoomController.java
@@ -0,0 +1,19 @@
+package com.codegym.rest_controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "*")
+@RequestMapping("/api/class-room")
+public class ClassRoomController {
+    @GetMapping()
+    public ResponseEntity<String> getText() {
+        String text = "Hello";
+        return new ResponseEntity<>(text, HttpStatus.OK);
+    }
+}
